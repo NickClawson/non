3e6d501c10ab59e239a7d51c67c47a96ccdaf790
Nick Clawson <NickClawson@fullsail.edu>
2012-06-25 21:28:02 -0400

    first commit

---
+++ js/json.js
@@ -0,0 +1,30 @@
+//author: Nick Clawson
+//Date: 6/19/2012
+//VFW 1206 Project: Coaster Rater
+
+var json = {
+	"ride1": {
+		"ride": ["Ride Name: ", "Raptor"],
+		"park": ["Park: ", "Cedar Point"],
+		"type": ["Ride Type: ", "RollerCoaster"],
+		"rating": ["Rating: ", "8"],
+		"date": ["Date Ridden: ", "6/19/2012"],
+		"comments": ["Comments: ", "classic ride! one of my all time favorites, but is starting to lose its shine."]
+	},
+	"ride2": {
+		"ride": ["Ride Name: ", "Maverick"],
+		"park": ["Park: ", "Cedar Point"],
+		"type": ["Ride Type: ", "RollerCoaster"],
+		"rating": ["Rating: ", "9"],
+		"date": ["Date Ridden: ", "6/19/2012"],
+		"comments": ["Comments: ", "new favorite! first hill inverts, very cool!"]
+	},
+	"ride3": {
+		"ride": ["Ride Name: ", "Max Air"],
+		"park": ["Park: ", "Cedar Point"],
+		"type": ["Ride Type: ", "Thrill"],
+		"rating": ["Rating: ", "1"],
+		"date": ["Date Ridden: ", "6/19/2012"],
+		"comments": ["Comments: ", "i don't care for it...too much spinning and back and forth motion, makes me feel ill."]
+	}
+}
